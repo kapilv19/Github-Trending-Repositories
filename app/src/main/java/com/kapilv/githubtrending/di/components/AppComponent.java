da2fef9e6abcb28f93f691f1df688d7f63f0e9aa
Kapil Verma <kapilv1999@gmail.com>
2022-05-02 16:58:08 +0530

    DI - Added Dummy Activity Module for Activity Injection

--- app/src/main/java/com/kapilv/githubtrending/di/components/AppComponent.java
+++ app/src/main/java/com/kapilv/githubtrending/di/components/AppComponent.java
@@ -3,6 +3,7 @@ package com.kapilv.githubtrending.di.components;
 import android.app.Application;
 
 import com.kapilv.githubtrending.app.GithubTrendingApp;
+import com.kapilv.githubtrending.di.modules.ActivityModule;
 import com.kapilv.githubtrending.di.modules.AppModule;
 import com.kapilv.githubtrending.di.modules.ModelRepositoryModule;
 import com.kapilv.githubtrending.di.modules.NetworkModule;
@@ -12,10 +13,13 @@ import javax.inject.Singleton;
 
 import dagger.BindsInstance;
 import dagger.Component;
+import dagger.android.AndroidInjectionModule;
 
 @Singleton
 @Component(
     modules = {
+        AndroidInjectionModule.class,
+        ActivityModule.class,
         AppModule.class,
         NetworkModule.class,
         ViewModelModule.class,
