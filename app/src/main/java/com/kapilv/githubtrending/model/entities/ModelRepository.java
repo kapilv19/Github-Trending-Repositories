38045019f3a0f21993cc0c48356cfddf65d9aad6
Kapil Verma <kapilv1999@gmail.com>
2022-05-03 05:02:45 +0530

    Integration - Changed Model Layer approach from Live data to Observable Observer(View Model) pattern and modified dependencies provided to be named for supporting force fetch from network

--- app/src/main/java/com/kapilv/githubtrending/model/entities/ModelRepository.java
+++ app/src/main/java/com/kapilv/githubtrending/model/entities/ModelRepository.java
@@ -1,89 +1,44 @@
 package com.kapilv.githubtrending.model.entities;
 
-import android.content.Context;
 import android.util.Log;
 
-import androidx.lifecycle.LifecycleOwner;
 import androidx.lifecycle.MediatorLiveData;
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.Observer;
 
-import com.kapilv.githubtrending.model.data.TrendingRepository;
+import com.kapilv.githubtrending.model.data.GitHubRepository;
 import com.kapilv.githubtrending.utils.network.TrendingRepositoriesService;
 
+import java.util.Arrays;
 import java.util.List;
 
 import javax.inject.Inject;
+import javax.inject.Named;
 
-import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers;
-import io.reactivex.rxjava3.annotations.NonNull;
 import io.reactivex.rxjava3.core.Single;
-import io.reactivex.rxjava3.core.SingleObserver;
-import io.reactivex.rxjava3.disposables.Disposable;
-import io.reactivex.rxjava3.schedulers.Schedulers;
 import retrofit2.Retrofit;
 
+//Model Layer of Application
 public class ModelRepository {
 
     private static final String TAG = ModelRepository.class.getSimpleName();
 
-    Retrofit mRetrofit;
-
-    private SingleObserver<List<TrendingRepository>> mObserver;
-
-    private TrendingRepositoriesService trendingRepositoriesService;
-    private Single<List<TrendingRepository>> mObservable;
-
-    private MutableLiveData<List<TrendingRepository>> mTrendingRepositories;
-    private MutableLiveData<Boolean> mError;
+    //retrofit Api service to do trending repositories network call
+    TrendingRepositoriesService mTrendingRepositoriesForcedService;
+    TrendingRepositoriesService mTrendingRepositoriesCachedService;
 
     @Inject
-    public ModelRepository(Retrofit retrofit) {
-        mRetrofit = retrofit;
-        init();
+    public ModelRepository(TrendingRepositoriesService trendingRepositoriesForcedService, TrendingRepositoriesService trendingRepositoriesCachedService) {
+        mTrendingRepositoriesForcedService = trendingRepositoriesForcedService;
+        mTrendingRepositoriesCachedService = trendingRepositoriesCachedService;
     }
 
-    public void subscribeToTrendingRepositories(MediatorLiveData<List<TrendingRepository>> mediator, Observer<List<TrendingRepository>> observer) {
-        mediator.addSource(mTrendingRepositories, observer);
-    }
-
-    public void subscribeToError(MediatorLiveData<Boolean> mediator, Observer<Boolean> observer) {
-        mediator.addSource(mError, observer);
-    }
-
-    public void fetchTrendingRepositories(boolean forceRefresh) {
-        if (forceRefresh) {
-            trendingRepositoriesService = mRetrofit.create(TrendingRepositoriesService.class);
-            mObservable = trendingRepositoriesService.getTrendingRepositories();
-            mObservable.subscribeOn(Schedulers.newThread())
-                    .observeOn(AndroidSchedulers.mainThread())
-                    .subscribe(mObserver);
-        } else {
-
+    //exposes method to fetch trending repositories
+    public Single<List<GitHubRepository>> fetchTrendingRepositories(boolean forceRefresh) {
+        if (forceRefresh) { //Force network call without cache
+            return mTrendingRepositoriesForcedService.getTrendingRepositories();
+        } else { //Network call with cache
+            return mTrendingRepositoriesCachedService.getTrendingRepositories();
         }
-    }
-
-    private void init() {
-        mTrendingRepositories = new MutableLiveData<>();
-        mError = new MutableLiveData<>();
-
-        mObserver = new SingleObserver<List<TrendingRepository>>() {
-            @Override
-            public void onSubscribe(@NonNull Disposable d) {
-                Log.e(TAG, "Subscribed to Retrofit Service");
-            }
-
-            @Override
-            public void onSuccess(@NonNull List<TrendingRepository> trendingRepositories) {
-                Log.e(TAG, "Retrofit Service fetch Successful");
-                mTrendingRepositories.postValue(trendingRepositories);
-            }
-
-            @Override
-            public void onError(@NonNull Throwable e) {
-                Log.e(TAG, "Retrofit Service Error: " + e.getMessage());
-                mError.postValue(true);
-            }
-        };
     }
 }
